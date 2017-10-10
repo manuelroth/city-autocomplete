2ee65e33af6faacb8f2e45d8ec1386bf2ad94bba
Manuel Roth <manuelroth@hotmail.ch>
2017-10-10 21:24:22 +0200

    Remove clean up export file function

--- src/export-city-index/export_city_index.sh
+++ src/export-city-index/export_city_index.sh
@@ -17,13 +17,8 @@ function export_city_index() {
   -a -f "/usr/src/app/export_city_index.sql"
 }
 
-function cleanup_index_file() {
-  sed -i '.original' 's/#//g' /usr/src/app/export/city_index.csv
-}
-
 function main() {
   export_city_index
-  cleanup_index_file
 }
 
 main
