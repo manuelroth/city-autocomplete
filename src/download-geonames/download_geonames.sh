0a8e2a6c7e76a123914106d63acbf244e71a6cfc
Manuel Roth <manuelroth@hotmail.ch>
2017-10-11 13:58:13 +0200

    Create component download-geonames which downloads and extracts geonames dataset

---
+++ src/download-geonames/download_geonames.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -o errexit
+set -o pipefail
+set -o nounset
+
+function prepare_geonames() {
+    wget -O geonames.zip http://download.geonames.org/export/dump/allCountries.zip
+    unzip geonames.zip
+    mv allCountries.txt ./import/geonames.txt
+    rm geonames.zip
+}
+
+function prepare_postalcodes() {
+    wget -O postalCodes.zip http://download.geonames.org/export/zip/allCountries.zip
+    unzip postalCodes.zip
+    mv allCountries.txt ./import/postalCodes.txt
+    rm postalCodes.zip
+}
+
+function prepare_countryInfo {
+    wget -O countryInfo.txt http://download.geonames.org/export/dump/countryInfo.txt
+    sed -i -e 1,51d countryInfo.txt
+    mv countryInfo.txt ./import/countryInfo.txt
+}
+
+function main() {
+  prepare_geonames
+  prepare_postalcodes
+  prepare_countryInfo
+}
+
+main
